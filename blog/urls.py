d661fff87b20d49283cac262ce2ad25a4f5b443a
Lando Vargas <lando_vargas@outlook.com>
2024-06-27 16:45:08 -0400

    feat: added PostDetail view to handle posts/:id

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from .views import PostView
+from .views import PostDetail
 
 
 urlpatterns = [
-    path('posts/', PostView.as_view())
+    path('posts/', PostView.as_view()),
+    path('posts/<int:pk>', PostDetail.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from django.http import Http404
 from .models import Post
 from .serializer import PostSerializer
 
@@ -24,5 +25,17 @@ class PostView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    @csrf_exempt
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
     
-    
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
