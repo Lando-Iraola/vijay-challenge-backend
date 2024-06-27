6633a029b4d0a6769f32e502b6b5fb2ccf8ee41c
Lando Vargas <lando_vargas@outlook.com>
2024-06-27 18:16:39 -0400

    feat: made titles unique as WordPress does the same

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, blank=False)
+    title = models.CharField(max_length=200, blank=False, unique=True)
     content = models.TextField(blank=False)
     author = models.CharField(max_length=100, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
