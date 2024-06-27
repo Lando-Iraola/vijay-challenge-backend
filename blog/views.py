ceb62188e0557b632b43f386836c8fd808dd7768
Lando Vargas <lando_vargas@outlook.com>
2024-06-27 08:15:50 -0400

    feat: added djangorestframework, created serialization, fixed model

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Post
+from .serializer import PostSerializer
 
 # Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+@csrf_exempt
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return render(request, 'Error Pages/405.html', status=405)
