1f56b07ed545ee0b676504ded055cecd1b25d2b1
Lando Vargas <lando_vargas@outlook.com>
2024-06-29 20:13:22 -0400

    feat: made the api request authentication, it also now expects the user when creating a post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200, blank=False, unique=True)
     content = models.TextField(blank=False)
-    author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
 from django.http import Http404
 from .models import Post
 from .models import Post
@@ -17,6 +18,7 @@ from django.contrib.auth.models import User
 
 
 class PostView(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     @csrf_exempt
     def get(self, request, format=None):
         posts = Post.objects.all()
@@ -26,13 +28,14 @@ class PostView(APIView):
         result_page = paginator.paginate_queryset(posts, request)
         serializer = PostSerializer(result_page, many=True)
         return paginator.get_paginated_response(serializer.data)
-        # return Response(serializer.data)
 
     @csrf_exempt
     def post(self, request, format=None):
         serializer = PostSerializer(data=request.data)
+        print(request.data)
+
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(author=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
